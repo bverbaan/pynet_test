e5cf71b1663390b3f9b2758e53a7c86940786746
bverbaan <bart.verbaandert@telenet.be>
2016-11-07 15:05:16 -0500

    list ex 3

---
+++ list_ex2.py
@@ -0,0 +1,11 @@
+my_list = range(5)
+print my_list
+my_list.append('element6')
+my_list.append('element7')
+print my_list
+my_list.pop(0)
+print my_list
+print "length is :{} ".format(len(my_list))
+my_list.sort()
+print my_list
+
