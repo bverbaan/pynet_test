f2eb61aadc2963bfe2f01231b0959f570fa29243
bverbaan <bart.verbaandert@telenet.be>
2016-11-07 15:23:43 -0500

    loop exercises

---
+++ loopex1.py
@@ -0,0 +1,11 @@
+my_list = range(1,50)
+
+print my_list
+
+for x in my_list:
+    if x == 13:
+	continue
+    print x
+    if x ==39:
+	break
+
---
+++ loopex2.py
@@ -0,0 +1,11 @@
+x = 0
+
+while x <= 49:
+    if x ==13:
+	x = x + 1
+	continue
+    print x
+    x = x + 1
+    if x == 39:
+	break
+
