44db4933259f92806bbd928137762428ed795148
bverbaan <bart.verbaandert@telenet.be>
2016-12-08 02:38:07 -0800

    excercises week 2

--- followup_exercises/week2/excercise3w2.py
+++ followup_exercises/week2/excercise3w2.py
@@ -16,13 +16,10 @@ def check_vlan_name(output, vlan_name):
     
     return vlan_name in output
 
-def add_vlan(net_connect, vlan_id, vlan_name=''):
-    cmd1 = "vlan " + str(vlan_id)
-    cmds = [cmd1,]
-    if vlan_name:
-        cmd2 = "name " + vlan_name
-        cmds.append(cmd2)
-    return net_connect.send_config_set(cmds)
+def remove_vlan(net_connect, vlan_id, vlan_name=''):
+    cmd1 = "no vlan " + str(vlan_id)
+
+    return net_connect.send_config_set(cmd1)
 
 
 def Check_vlan_exists(output):
@@ -48,12 +45,10 @@ def ssh_conn(a_device, my_queue):
 	    output = net_conn.send_command(cmd)
 	    print output
 	    vlan_exists = check_vlan_exists(output)
-	    print "vlan exists is {}".format(vlan_exists) 
-	    vlan_name_correct = check_vlan_name(output, vlan_name)
-	    print "vlan name correct is {}".format(vlan_name_correct)
-	    if not vlan_exists or not vlan_name_correct:
-		print "Updating and/or adding vlan"
-		output = add_vlan(net_conn, vlan_id, vlan_name)
+	    print "vlan exists is {}".format(vlan_exists)
+	    if vlan_exists:
+		print "removing vlan"
+		output = remove_vlan(net_conn, vlan_id)
 		output_dict[device_name][vlan_id] = 'changed'
                 
             else:
